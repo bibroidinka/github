a5190c86d01729372874959694db3510ae3dc9ba
bibroidinka <115043830+bibroidinka@users.noreply.github.com>
2025-05-07 18:09:51 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 local uiModule = require(script.Parent.ui)  -- Получаем модуль с интерфейсом
 local screenui = uiModule.ui  -- Доступ к объекту ScreenGui через module.ui
 
-local guiLogic = require(script.Parent.guiLogic)  -- Подключаем логику
+local guiLogic = require(script.Parent.init)  -- Подключаем логику
 guiLogic.init(screenui)  -- Передаем screenui в логику интерфейса
