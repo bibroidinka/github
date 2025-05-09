cf93766c861f8eb941cd2fb24a96cc615980793d
bibroidinka <115043830+bibroidinka@users.noreply.github.com>
2025-05-09 16:33:20 +0200

    Update TpNpc.lua

--- Features/TpNpc.lua
+++ Features/TpNpc.lua
@@ -12,13 +12,13 @@ local Humanoid = Character:WaitForChild("Humanoid")
 
 
 -- функция для передвижения игрока
-function moveCharacter(character)
+function moveCharacter(character,TargetPositiones)
     CollisionPlayer(character,false)
 
 
     local root = character:WaitForChild("HumanoidRootPart")
     local tweenInfo = TweenInfo.new(25, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
-    local targetPosition = Vector3.new(100, 15, 0)
+    local targetPosition = Vector3.new(TargetPositiones)
     local tween = tweenService:Create(root, tweenInfo, {CFrame = CFrame.new(targetPosition)})
     tween:Play()
     completed_tp = false
@@ -41,7 +41,7 @@ end
 function module.Setup(Tp_button)
     Tp_button.MouseButton1Click:Connect(function()
         if completed_tp then
-            moveCharacter(localPlayer.Character)
+            moveCharacter(localPlayer.Character,0,100,5)
         else
             print("Вы еще не долетели до места")
         end
